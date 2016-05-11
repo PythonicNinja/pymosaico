bd99fd0cce1402191ae650f745dc322d16a6178d
Wojciech Nowak <mail@pythonic.ninja>
2016-05-11 17:22:30 +0200

    initial structure of django app

---
+++ server/images/models.py
@@ -0,0 +1,28 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+
+
+class Image(TimeStampedModel):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    name = models.CharField(verbose_name=_('Name'), max_length=255)
+    image = models.ImageField(verbose_name=_('Image'), upload_to='images/%Y/%m/%d')
+
+
+class Image2Folder(TimeStampedModel):
+    image = models.ForeignKey('Image')
+    folder = models.ForeignKey('ImageFolder')
+    ordering = models.PositiveSmallIntegerField(verbose_name=_('Ordering'))
+
+    class Meta:
+        ordering = ('ordering',)
+        unique_together = ('folder', 'ordering')
+
+
+class ImageFolder(TimeStampedModel):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    name = models.CharField(verbose_name=_('Name'), max_length=255)
+    images = models.ManyToManyField('Image', through=Image2Folder, verbose_name=_('Images'))
